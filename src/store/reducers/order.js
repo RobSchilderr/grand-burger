21c26af070f70a497453855ae19b526db86decaa
RobSchilderr <35261620+RobSchilderr@users.noreply.github.com>
2020-10-27 14:46:03 +0100

    Added more redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,15 +3,19 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState = {
   orders: [],
   loading: false,
+  purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.PURCHASE_INIT:
+      return { ...state, purchased: false };
+
     case actionTypes.PURCHASE_BURGER_START:
-    return {
-      ...state,
-      loading:true,
-    }
+      return {
+        ...state,
+        loading: true,
+      };
     case actionTypes.PURCHASE_BURGER_SUCCESS:
       const newOrder = {
         ...action.orderData,
@@ -20,14 +24,23 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        orders: state.orders.concat(
-          newOrder
-        ),
+        purchases: true,
+        orders: state.orders.concat(newOrder),
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
       return {
-        ...state, loading: false
+        ...state,
+        loading: false,
       };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return { ...state, orders: state.orders, loading: false };
+    case actionTypes.PURCHASE_BURGER_FAIL:
+      return { ...state, loading: false };
     default:
       return state;
   }
